b1350835e028cff8d9429936aee347345998160f
ThePotato <10260415+ThePotato97@users.noreply.github.com>
2021-12-07 18:00:15 +0000

    make got request first

--- index.js
+++ index.js
@@ -107,8 +107,8 @@ async function getVisits(userId) {
 
 
 app.get("/getPlaceVisits", (req, res, next) => {
+    console.log("Got request: " + req.query.userId)
     getVisits(req.query.userId).then(visits => {
-        console.log("Got request: " + req.query.userId)
         res.json(visits);
     })
 });
