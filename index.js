061f044b4db959c82510f38b66ac6a81a7cd32ff
ThePotato <10260415+ThePotato97@users.noreply.github.com>
2021-12-07 18:57:28 +0000

    fix typo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}}`)
+    console.log(`Server started on port ${PORT}`)
 })
 
 noblox.setCookie("***REMOVED***").then(function() { //Use COOKIE from our .env file.
